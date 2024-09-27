415d4a8c852f1d93495aca426af14a3f7a393cda
tech15004 <techknowlogic10@gmail.com>
2024-09-26 20:18:43 -0400

    Driver operator code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOperator.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+//@Disabled
+@TeleOp
+
+public class DriverOperator extends OpMode {
+
+    // Declaring the DC Motors
+    DcMotor frontLeft = null;
+    DcMotor frontRight = null;
+    DcMotor backLeft = null;
+    DcMotor backRight= null;
+    int drivePower = 2;
+
+    @Override
+    public void init(){
+
+        // Initializing the DC Motors
+        frontLeft = hardwareMap.dcMotor.get("frontleft"); // Port #0
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontRight = hardwareMap.dcMotor.get("frontright"); // Port #1
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        backLeft = hardwareMap.dcMotor.get("backleft"); //Port#2
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        backRight = hardwareMap.dcMotor.get("backright"); //Port #3
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+    @Override
+    public void loop(){
+
+        // Taking the values from the Gamepad/Joysticks
+        double y = gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x * 1.1;
+        double rx = gamepad1.right_stick_x;
+
+        // Normalizing the Power
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx),drivePower);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double frontRightPower = (y - x + rx) / denominator;
+        double backLeftPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        //Setting the Power for the DC Motors
+        frontLeft.setPower(frontLeftPower);
+        frontRight.setPower(frontRightPower);
+        backLeft.setPower(backLeftPower);
+        backRight.setPower(backRightPower);
+
+
+
+
+    }
+}
